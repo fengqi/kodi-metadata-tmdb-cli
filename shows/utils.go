048ae019ef7eca64f123de78f9da40f7210dff81
fengqi <lyf362345@gmail.com>
2022-12-29 02:43:32 +0000

    空文件下载判断

--- shows/utils.go
+++ shows/utils.go
@@ -254,7 +254,7 @@ func (d *Dir) downloadImage(detail *tmdb.TvDetail) {
 	// TODO group的信息里可能 season poster不全
 	if len(detail.Seasons) > 0 {
 		for _, item := range detail.Seasons {
-			if !d.IsCollection && item.SeasonNumber != d.Season {
+			if !d.IsCollection && item.SeasonNumber != d.Season || item.PosterPath == "" {
 				continue
 			}
 			seasonPoster := fmt.Sprintf("season%02d-poster.jpg", item.SeasonNumber)
