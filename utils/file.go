f1ca2d5ce38decf368e55cbaddd66a5e943f6b44
fengqi <lyf362345@gmail.com>
2023-01-01 09:02:11 +0000

    新增复制文件方法

--- utils/file.go
+++ utils/file.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -70,4 +71,22 @@ func DownloadFile(url string, filename string) error {
 	}
 
 	return nil
+}
+
+func CopyFile(dstName, srcName string) (writeen int64, err error) {
+	src, err := os.Open(dstName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer src.Close()
+
+	dst, err := os.OpenFile(srcName, os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer dst.Close()
+
+	return io.Copy(dst, src)
 }
