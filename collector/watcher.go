f034b234eed7b901e7cde37b2dd7bb8862dac4c8
fengqi <lyf362345@gmail.com>
2025-06-01 08:42:29 +0000

    补全变动监听

--- collector/watcher.go
+++ collector/watcher.go
@@ -1,10 +1,54 @@
 package collector
 
 import (
+	"fengqi/kodi-metadata-tmdb-cli/config"
+	"fengqi/kodi-metadata-tmdb-cli/media_file"
+	"fengqi/kodi-metadata-tmdb-cli/utils"
 	"os"
+	"strings"
 )
 
 // watcherCallback 监听文件变化的回调函数
-func (c *collector) watcherCallback(filename string, fileInfo os.FileInfo) {
-	//	log.Println(filename, fileInfo)
+// todo 部分逻辑和scanDir重复，考虑复用
+func (c *collector) watcherCallback(filename string, fi os.FileInfo) {
+	if fi.Name()[0:1] == "." {
+		return
+	}
+
+	if fi.IsDir() {
+		if c.skipFolders(filename, fi.Name()) {
+			utils.Logger.DebugF("skip folder by config: %s", fi.Name())
+			return
+		}
+
+		c.watcher.Add(filename)
+	}
+
+	var videoType media_file.VideoType
+	for _, item := range config.Collector.MoviesDir {
+		if strings.HasPrefix(filename, item) {
+			videoType = media_file.Movies
+			break
+		}
+	}
+	for _, item := range config.Collector.ShowsDir {
+		if strings.HasPrefix(filename, item) {
+			videoType = media_file.TvShows
+			break
+		}
+	}
+	for _, item := range config.Collector.MusicVideosDir {
+		if strings.HasPrefix(filename, item) {
+			videoType = media_file.MusicVideo
+			break
+		}
+	}
+
+	mf := media_file.NewMediaFile(filename, fi.Name(), videoType)
+	if mf.IsBluRay() {
+		c.channel <- mf
+	}
+	if mf.IsVideo() {
+		c.channel <- mf
+	}
 }
